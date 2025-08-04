0e086dcae9db49eb1b912befd43922eb9d234d1a
moranenzo <enzo.moran337@gmail.com>
2025-08-04 07:58:35 +0000

    Add __init__

---
+++ src/__init__.py
@@ -0,0 +1,21 @@
+"""
+src package initializer.
+
+This package includes modules for clustering methods and distribution utilities,
+primarily used in Wasserstein-based clustering of income distributions.
+"""
+
+# Explicit imports for convenience
+from .clustering_methods import (
+    bary_WKMeans,
+    dist_WKMeans
+)
+
+from .utils import (
+    reconstruct_joint_distribution_ot,
+    create_regular_grid,
+    project_distribution_on_grid,
+    computeDistanceMatrix,
+    compute_barycenter_for_cluster,
+    plot_projected_distribution
+)
